76f80aa172bee7237a8f9b17c1ca574885bbf120
Enkhtur <neo.orgil@gmail.com>
2024-02-14 01:59:08 +0800

    Revel feature-g buren hiij duusgav

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import "../styles/styles.css";
-// const MobileMenu = require("./module/MobileMenu.js");
-import MobileMenu from "./module/MobileMenu.js";
+import MobileMenu from "./modules/MobileMenu.js";
+import RevealOnScroll from "./modules/RevealOnScroll.js";
 // object uusgey
-let mobileMenu = new mobileMenu();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
+let mobileMenu = new MobileMenu();
 // webpack-g maani watch hiij bga code
 if (module.hot) {
   module.hot.accept();
