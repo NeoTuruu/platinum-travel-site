ff6cbc0603d14f6d1185155cbea3fd57257fa2f4
Enkhtur <neo.orgil@gmail.com>
2024-02-16 23:25:47 +0800

    build process-iin tohirgoog hiij duuslaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,6 +7,7 @@ import StickyHeader from "./modules/StickyHeader.js";
 
 // object uusgey
 // new Modal();
+
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
